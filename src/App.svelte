404a39e0177ad2352b00fe3242d40edce95d43f0
Sebastian Rendon <7554810-serendongi@users.noreply.gitlab.com>
2021-08-23 16:43:03 -0500

    chores: example NestJS request

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,27 @@
 <script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import { onMount } from 'svelte';
+  import ky from 'ky';
+  import logo from './assets/svelte.png';
+  import Counter from './lib/Counter.svelte';
+
+  let text = 'Connecting to NestJS server...';
+
+  onMount(async () => {
+    try {
+      text = await ky.get('http://localhost:8000/').text();
+    } catch (error) {
+      text = 'Could not connect to NestJS server 😞';
+    }
+  });
 </script>
 
 <main>
   <img src={logo} alt="Svelte Logo" />
   <h1>Hello Typescript!</h1>
-
   <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  {#if text}
+    <p>{text}</p>
+  {/if}
 </main>
 
 <style>
